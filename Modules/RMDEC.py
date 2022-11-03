986323b4c64fcc87fafbc670a7b79d9a70535999
G.S.Sanjay <110849848+sanjay021104gs@users.noreply.github.com>
2022-11-03 19:56:18 +0530

    Add files via upload

---
+++ Modules/RMDEC.py
@@ -0,0 +1,7 @@
+import RMD
+a=int(input("Enter a number : "))
+b=int(input("Enter another number : "))
+print("sum=",RMD.add(a,b))
+print("multiplication = ",RMD.mult(a,b))
+print("subtraction =",RMD.sub(a,b))
+print("division =" , RMD.div(a,b))
