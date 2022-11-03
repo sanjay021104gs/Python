e45560fadc6e9069cbd387ced2918568867e9989
G.S.Sanjay <110849848+sanjay021104gs@users.noreply.github.com>
2022-11-03 19:55:53 +0530

    Add files via upload

---
+++ Modules/stu.py
@@ -0,0 +1,6 @@
+Lavade ={
+    "Name":"Sanjay",
+    "Age":18,
+    "College":"RMD"
+   
+}
---
+++ Modules/student.py
@@ -0,0 +1,7 @@
+import stu as dabra
+stname=dabra.Lavade["Name"]
+aage=dabra.Lavade["Age"]
+clg=dabra.Lavade["College"]
+print(stname)
+print(aage)
+print(clg)
